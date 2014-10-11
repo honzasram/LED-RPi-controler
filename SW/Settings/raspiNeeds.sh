6c93612ce0fe182b95aade8da086e1dd32e1916f
Jan Šrámek <honzasram93@gmail.com>
2014-10-11 18:55:54 +0200

    Update raspiNeeds.sh

--- SW/Settings/raspiNeeds.sh
+++ SW/Settings/raspiNeeds.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 #this is script which will prepare your RPi to work with module via I2C bus and also it will set up other things like web server, cron table, etc.
 
+
+
+cat '# blacklist spi and i2c by default (many users dont need them)' > /etc/modprobe.d/raspi-blacklist.conf
+cat 'blacklist spi-bcm2708' >> /etc/modprobe.d/raspi-blacklist.conf
+cat 'i2c-dev' >> /etc/modules
+
+
+sudo crontab -l > var
+echo '@reboot sleep 10 &&/home/pi/.run &' >> $var
+sudo crontab $var
+
